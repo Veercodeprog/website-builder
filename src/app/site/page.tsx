18e91e2ff090c33e214cba229512cea17ee68d2d
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-08-15 11:08:53 +0530

    homepage updates

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col ">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
       <p className="text-center mt-10">your agency in one place </p>
-      <div className="bg-gradient-to-r  bg-clip-text relative">
+      <div className="bg-gradient-to-r   bg-clip-text relative">
         <h1 className="text-9xl font-bold text-center md:text-[300px]">
           builder{" "}
         </h1>
@@ -23,5 +23,21 @@ export default function Home() {
       />
       <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
     </section>
-  );
+<section className= " flex justify-center items-center flex-col gap-4 md:mt-20" >
+
+<h2 className="text-4xl text-center">
+choose what fits you right</h2>
+                    <p className="text-muted-foreground text-center ">
+      Our simple pricing plans are designed to fit your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+
+
+                        </p>
+
+
+
+        </section>
+
+    );
 }
